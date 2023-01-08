5fa9031d5e83d3967ba31cf74876439d04d8dfc5
OrnelaDanushi <50953998+OrnelaDanushi@users.noreply.github.com>
2023-01-08 14:12:43 +0100

    Update jumping_creatures.pl

--- jumping_creatures.pl
+++ jumping_creatures.pl
@@ -1,16 +1,22 @@
 % assuming that
+
 % all jumping creatures are green
-% all small jumping creatures are Martians
-% all green Martians are intelligent
-% Ngtrks is small and green
-% Pgvdrk is a jumping Martian
-% who is intelligent?
 green(X) :- jumping(X) .
+
+% all small jumping creatures are Martians
 martian(X) :- small(X), jumping(X) .
+
+% all green Martians are intelligent
 intelligent(X) :- green(X), martian(X) .
+
+% Ngtrks is small and green
 small(ngtrks) .
 green(ngtrks) .
+
+% Pgvdrk is a jumping Martian
 jumping(pgvdrk) .
 martian(pgvdrk) .
+
+% who is intelligent?
 % ?- intelligent(W). 		W = pgvdrk
 % ?- martian(ngtrks).		false
